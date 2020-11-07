0fb2b59d19fcca5a1514ff630c7815fe6cf278a4
Gustavo-sg01 <Suastegui_sg1997@live.com>
2020-11-07 00:30:47 -0600

    Maty

--- resources/views/Nosotros.blade.php
+++ resources/views/Nosotros.blade.php
@@ -18,7 +18,9 @@
                  <p>Que Paso papa!!!Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                      Corporis libero, eaque dolores laudantium earum amet eveniet rem, 
                      ipsam magni alias officia quos, facere nisi nostrum debitis eligendi 
-                     impedit veritatis inventore.</p>
+                     impedit veritatis inventore.
+                  Matilll
+                  </p>
                  
           @break
 
